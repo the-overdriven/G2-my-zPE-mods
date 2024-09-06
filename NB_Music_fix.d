775652d575fbc0897799b03932634693a3c77fa0
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-09-06 20:49:52 +0200

    Update NB_Music_fix.d

--- NB_Music_fix.d
+++ NB_Music_fix.d
@@ -9,7 +9,6 @@ instance NWP_DAY_STD(C_MUSICTHEME_STANDARD)
 	// New World Pyramids
 	// Returning: "Keeper_library.sgt"
 	file = "MI_DayStd.sgt";
-	// "MAY_DayStd.sgt";
 };
 
 instance MOD_NGT_STD(C_MUSICTHEME_STANDARD)
