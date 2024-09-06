14f79eb337ff839a3832b0a17f3c6c3433676da3
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-09-06 20:45:00 +0200

    Add files via upload

---
+++ InvisibleLight.d
@@ -0,0 +1,20 @@
+META
+{
+    Parser = PFX
+    MergeMode = TRUE
+};
+
+INSTANCE MFX_LIGHT_INIT (C_PARTICLEFX) {
+    MFX_LIGHT_INIT_OLD();
+};
+
+INSTANCE MFX_LIGHT_ORIGIN (C_PARTICLEFX) {
+    MFX_LIGHT_ORIGIN_OLD();
+    vistexcolorstart_s = "0";
+};
+
+INSTANCE MFX_LIGHT_ORIGIN_LESS (C_PARTICLEFX)
+{
+    MFX_LIGHT_ORIGIN_LESS_OLD();
+    vistexcolorstart_s = "0";
+};
