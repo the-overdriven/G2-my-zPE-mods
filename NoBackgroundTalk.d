1e1627dc5942da30a12ea6de8be508a995f28d60
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-09-09 16:10:40 +0200

    Add files via upload

---
+++ NoBackgroundTalk.d
@@ -0,0 +1,17 @@
+/*
+Mutes:
+- small talk between all NPC
+- Vatras preaching
+- Herold announcements
+*/
+META
+{
+    Parser = Game
+    MergeMode = TRUE
+};
+
+func void b_preach_vatras() { };
+
+func void b_announce_herold() { };
+
+func void b_say_smalltalk() { };
